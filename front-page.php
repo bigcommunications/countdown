98bbaf0678d91a77ee0aa3cb297ff47485a18502
Dan Gavin <iamdangavin@gmail.com>
2013-06-17 16:31:11 -0500

    Live Filter

--- front-page.php
+++ front-page.php
@@ -1,6 +1,14 @@
+<section class="row countdown-filter">
+	<div class="large-11 columns">
+		<input type="text" data-filter data-filter-target="countdown-holster" placeholder="Filter projects..." />
+	</div>
+	<div class="large-1 columns search">
+		<i class="icon-search"></i>
+	</div>
+</section>
 <section id="bars" class="row count-down-bars">
 
 </section>
-<section class="row countdown-holster">
+<section class="row countdown-holster" data-filter-items="countdown-holster">
 
 </section>
